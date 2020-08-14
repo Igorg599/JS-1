9edac93fb79ce71dd5dcc9f86bec9d89d61d4716
Igor Gordeev <igorg599@gmail.com>
2020-08-14 19:59:49 +0500

    first

---
+++ js/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// const obj = {
+//     name: "Ivan",
+//     age: 11,
+//     isMarried: false
+// };
+
+// console.log(obj.isMarried);
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как вашf фамилия?', '');
+// answers[2] = prompt('сколько вам лет?', '');
+
+// document.write(answers);
+const user = "Ivan";
+alert(`Hi, ${user}`);
